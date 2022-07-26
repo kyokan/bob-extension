a54a31225349d5f3c014e045c11b32f427cbf9f8
Casey K <tehcnicolour@gmail.com>
2022-07-25 22:59:00 -0700

    non-conflicting node-sass and sass-loader versions

--- webpack.common.js
+++ webpack.common.js
@@ -32,6 +32,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      },
+      {
         exclude: /node_modules/,
         test: /\.tsx?$/,
         use: "ts-loader",
